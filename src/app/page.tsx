a0bd35ee4baf43968457df3c8adee3762789e154
nick <nickt1107@gmail.com>
2024-10-16 21:08:12 -0400

    updated images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,21 @@ export default function Home() {
   return (
     <main>      
       <body className="min-h-screen flex flex-col justify-center items-center text-center">
-          <h1 className="text-4xl font-bold">Nicholas Tranquilli</h1>
+          <h1 className="title">Nicholas Tranquilli</h1>
           <div className="flex space-x-4">
               <div className="flex-shrink-0">
                   <a href="https://github.com/NicholasTranquilli">
-                      <Image src="resources/github.svg" alt="GitHub"/>
+                      <Image src="../resources/github.svg" alt="GitHub"/>
                   </a>
               </div>
               <div className="flex-shrink-0">
                   <a href="https://www.linkedin.com/in/nicholas-tranquilli-9b2135284/">
-                      <Image src="resources/linkedin.svg" alt="LinkedIn"/>
+                      <Image src="../resources/linkedin.svg" alt="LinkedIn"/>
                   </a>
               </div>
               <div className="flex-shrink-0">
                   <a href="mailto:nicktranquilli7@gmail.com">
-                      <Image src="resources/email.svg" alt="Email"/>
+                      <Image src="../resources/email.svg" alt="Email"/>
                   </a>
               </div>
           </div>
