265fdd4a362098265555f75a116982594ba0d615
nick <nickt1107@gmail.com>
2024-10-16 21:01:57 -0400

    bootstrap to tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,23 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>      
-      <body>
-          <div className="min-vh-100 d-flex flex-column justify-content-center text-center">
-              <h1 className="title">Nicholas Tranquilli</h1>
-              <div className="link-slots">
-                  <div className="link-slot">
-                      <a href="https://github.com/NicholasTranquilli">
-                          <Image src="resources/github.svg" alt="..."/>
-                      </a>
-                  </div>
-                  <div className="link-slot">
-                      <a href="https://www.linkedin.com/in/nicholas-tranquilli-9b2135284/">
-                          <Image src="resources/linkedin.svg" alt="..."/>
-                      </a>
-                  </div>
-                  <div className="link-slot">
-                      <a href="/contact">
-                          <Image src="resources/email.svg" alt="..."/>
-                      </a>
-                  </div>
+      <body className="min-h-screen flex flex-col justify-center items-center text-center">
+          <h1 className="text-4xl font-bold">Nicholas Tranquilli</h1>
+          <div className="flex space-x-4">
+              <div className="flex-shrink-0">
+                  <a href="https://github.com/NicholasTranquilli">
+                      <Image src="resources/github.svg" alt="GitHub"/>
+                  </a>
+              </div>
+              <div className="flex-shrink-0">
+                  <a href="https://www.linkedin.com/in/nicholas-tranquilli-9b2135284/">
+                      <Image src="resources/linkedin.svg" alt="LinkedIn"/>
+                  </a>
+              </div>
+              <div className="flex-shrink-0">
+                  <a href="mailto:nicktranquilli7@gmail.com">
+                      <Image src="resources/email.svg" alt="Email"/>
+                  </a>
               </div>
           </div>
       </body>
