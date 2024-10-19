35c518fdfc270316baa5a4b86ad6b5183b6803a3
nick <nickt1107@gmail.com>
2024-10-19 02:54:12 -0400

    simple front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,45 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import Image from "next/image";
+import * as THREE from 'three'
+import React, { useRef, useState } from 'react'
+import { Canvas, useThree, useFrame, ThreeElements } from '@react-three/fiber'
+import { extend, Object3DNode } from '@react-three/fiber'
+import { Font, FontLoader } from 'three/addons/loaders/FontLoader.js';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+import { error } from "console";
+import { useGLTF, useTexture, Center, Decal, Text3D, OrbitControls, Environment, MeshReflectorMaterial } from '@react-three/drei'
+import { update } from "three/examples/jsm/libs/tween.module.js";
+import { Lobster } from "../components/Lobster";
+import { tree } from "next/dist/build/templates/app-page";
+
+export default function App() {
+  var linkImgSize = 20;
   return (
-    <main>      
-      <body className="min-h-screen flex flex-col justify-center items-center text-center">
-          <h1 className="title">Nicholas Tranquilli</h1>
-          <div className="flex space-x-4">
-              <div className="flex-shrink-0">
-                  <a href="https://github.com/NicholasTranquilli">
-                      <Image src="../resources/github.svg" alt="GitHub"/>
-                  </a>
-              </div>
-              <div className="flex-shrink-0">
-                  <a href="https://www.linkedin.com/in/nicholas-tranquilli-9b2135284/">
-                      <Image src="../resources/linkedin.svg" alt="LinkedIn"/>
-                  </a>
-              </div>
-              <div className="flex-shrink-0">
-                  <a href="mailto:nicktranquilli7@gmail.com">
-                      <Image src="../resources/email.svg" alt="Email"/>
-                  </a>
-              </div>
-          </div>
-      </body>
-    </main>
-  );
+    <div className="flex min-h-screen justify-center items-center ">
+    <div className="text-center">
+      <div className="text-6xl">Nicholas Tranquilli</div>
+      <div className="flex justify-center text-center text-2xl my-3">
+        <div className="flex-1">
+          <a href="https://github.com/NicholasTranquilli">
+            <Image className="mx-2" style={{whiteSpace:'nowrap', display:'inline'}} width={linkImgSize} height={linkImgSize} src="/resources/github.svg" alt="..."></Image>
+            Github
+          </a>
+        </div>
+        <div className="flex-1">
+          <a href="https://www.linkedin.com/in/nicholas-tranquilli/">
+            <Image className="mx-2" style={{whiteSpace:'nowrap', display:'inline'}} width={linkImgSize} height={linkImgSize} src="/resources/linkedin.svg" alt="..."></Image>
+            LinkedIn
+          </a>
+        </div>
+        <div className="flex-1">
+          <a href="Emailto:nicholast@my.ccsu.edu">
+            <Image className="mx-2" style={{whiteSpace:'nowrap', display:'inline'}} width={linkImgSize} height={linkImgSize} src="/resources/email.svg" alt="..."></Image>
+            Email
+          </a>
+        </div>
+      </div>
+    </div>
+    </div>
+  )
 }
